f35b74e549cbca4320bafabb52f940ef69c3904e
MirasTolebek <miras.546266@gmail.com>
2026-06-17 10:20:07 +0500

    Создан файл secrets.h для хранения конфиденциальных данных
    Добавлен файл secrets.h с настройками Wi-Fi и Telegram.

---
+++ secrets.h
@@ -0,0 +1,9 @@
+// Файл secrets.h - Здесь хранятся все пароли и настройки
+
+// Настройки Wi-Fi
+const char* ssid     = "ВАШ_НОВЫЙ_WIFI"; 
+const char* password = "ВАШ_НОВЫЙ_ПАРОЛЬ";  
+
+// Настройки Telegram
+#define BOTtoken "ВАШ_НОВЫЙ_ТОКЕН_БОТА"
+#define CHAT_ID  "ВАШ_НОВЫЙ_CHAT_ID"
